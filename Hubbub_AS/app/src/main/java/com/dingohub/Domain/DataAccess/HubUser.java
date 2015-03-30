74b91b5a6b4a5432d6d4fe7cb29692b26923b26e
ereio <git@ere.io>
2015-03-30 07:57:39 -0400

    Adding gitignore props

---
+++ Hubbub_AS/app/src/main/java/com/dingohub/Domain/DataAccess/HubUser.java
@@ -0,0 +1,29 @@
+package com.dingohub.Domain.DataAccess;
+
+import org.json.JSONArray;
+
+public class HubUser {
+	
+	public String id;
+	public String username;
+	public String email;
+	public String firstname;
+	public String lastname;
+	public String details;
+	public String location;
+	public byte[] picture;
+	public JSONArray friend_ids;
+	
+	
+	public boolean SendFriendRequest(){
+		return true;
+	}
+	
+	public boolean AcceptFriendRequest(){
+		return true;
+	}
+	
+	public String[] FriendsList(){
+		return null;
+	}
+}
